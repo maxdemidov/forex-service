c540a272e9710d27450c78a445b6eb37d741123f
mdemidov <maxim.a.demidov@gmail.com>
2020-07-08 15:51:18 +0300

    fix README, and add comments to test

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 # forex-service
 ---
 Hello!
-As we have limitations to call one-frame API, we need some cache in the middle between our client's gets and the frame.
-We can get all rates by one call and cache them.
-Also, it would be nice to have some eager mechanism to pull data from the frame before it expired in order to protect our clients from long calls for fresh rates to frame directly.
-But on the other way we may refresh those rates for nobody, so it also nice to have some mechanism that checks activity and switch from eager autorefresh to lazy - that starts to call frame API in case of activity.
-The other concern I handle is some retry flow in case we can't get a date from the frame or get an error.
-I extend API with bid and ask and create an endpoint to get a list of rates by list of pairs in one-frame's API manner.
+There is a lot to say in details but I describe only the main most important concerns.
+#
+About implementation:
+As we have limitation to call one-frame API, we need some cache in the middle between our client's gets and the frame.
+We can get all rates by one call to the frame and cache them for some period of time.
+Also, it would be nice to have some eager mechanism to pull data from the frame before it expired in order to protect our clients from long calls to refresh rates from frame directly.
+But on the other way we may refresh those rates for nobody, so it also nice to have some mechanism that checks activity and switch from eager autorefresh to lazy - that sleeps and starts to call frame API only by demand. 
+So we can switch cache from lazy, for example at night, to eager refresh mode in case of continuous activity were performed during the day.
+The other concern which is nice to handle is some retry flow in case we can't get rates from the frame or get an error.
+This is how it's done.
+#
+About API:
+I extended API with bid and ask fields, and create an endpoint to get a list of rates by list of pairs in one-frame's API manner.
+Also, there are some additional checks for query params. And endpoint to return list of supported currencies.
 #
 About structure:
 CacheProgram responses for refreshing its underlying cache and switch between modes I've mentioned about.
@@ -20,5 +28,6 @@ Cache use next config params:
 `refresh-timeout` - it should be less then expiration-timeout, less enough to process a regular call before expiration happened, to refresh rates from frame under the scene for our clients, for example, 4 minutes.
 `wait-timeout` - a timeout we waiting for a call from the frame before considering its as unreachable, should be less then subtraction between expiration and refresh but enough to perform a regular call over the network, for example, 10 seconds. 
 #
-I'm working on some tests for checking retry mechanism
-And there a couple of Todo's where I've been planning to do improvements       
+I'm working on some tests for checking the retry mechanism.
+And there a couple of Todo's where I've been planning improvements.
+I hope You will have had some comments, and I ready to listen.
--- src/test/scala/forex/cache/CacheProgramStateTest.scala
+++ src/test/scala/forex/cache/CacheProgramStateTest.scala
@@ -36,6 +36,7 @@ class CacheProgramStateTest extends AnyFunSuite {
     )
     val cacheEnvIO = for {
       counter <- Ref[IO].of(0)
+      _ <- logger.info("Set all pairs state by default")
       rates <- Ref[IO].of(CacheProgramHelper.allPairs)
       duration <- Ref[IO].of(100.milliseconds)
       ratesService: ModifiedCache[IO] = new OneFrameModified[IO](counter, rates, duration)
@@ -46,6 +47,7 @@ class CacheProgramStateTest extends AnyFunSuite {
       cacheEnv <- cacheEnvIO
       _ <- timer.sleep(3.seconds)
       calls <- cacheEnv.counter.get
+      _ <- logger.info("Check that frame was touched only once at the start")
       _ = assert(calls == 1)
     } yield ()
     callsIO.unsafeRunSync()
@@ -59,6 +61,7 @@ class CacheProgramStateTest extends AnyFunSuite {
     )
     val cacheEnvIO = for {
       counter <- Ref[IO].of(0)
+      _ <- logger.info("Set all pairs state by default")
       rates <- Ref[IO].of(CacheProgramHelper.allPairs)
       duration <- Ref[IO].of(100.milliseconds)
       ratesService = new OneFrameModified[IO](counter, rates, duration)
@@ -67,16 +70,19 @@ class CacheProgramStateTest extends AnyFunSuite {
 
     val resultsIO = for {
       cacheEnv <- cacheEnvIO
+      _ <- timer.sleep(3.seconds)
+      _ <- logger.info("Perform four calls to the cache")
       rate1 <- cacheEnv.cacheService.get(List(pairUSDEUR))
       rate2 <- cacheEnv.cacheService.get(List(pairUSDAUD))
       rate3 <- cacheEnv.cacheService.get(List(pairAUDJPY))
       rate4 <- cacheEnv.cacheService.get(List(nonexistentPair))
+      _ <- logger.info("Check that they processed correctly")
       _ = assert(rate1.getOrElse(throw CacheProgramHelper.tryToGetLeft) == List(rateUSDEUR))
       _ = assert(rate2.getOrElse(throw CacheProgramHelper.tryToGetLeft) == List(rateUSDAUD))
       _ = assert(rate3.getOrElse(throw CacheProgramHelper.tryToGetLeft) == List(rateAUDJPY))
-      _ = assert(rate4.left.exists(
-        error => error == RateNotFound(errors.Messages.notFoundRateMessage(nonexistentPair))))
+      _ = assert(rate4.left.exists(error => error == RateNotFound(errors.Messages.notFoundRateMessage(nonexistentPair))))
       _ <- timer.sleep(3.seconds)
+      _ <- logger.info("Check that frame was touched only once")
       calls <- cacheEnv.counter.get
       _ = assert(calls == 1)
     } yield ()
@@ -92,6 +98,7 @@ class CacheProgramStateTest extends AnyFunSuite {
     )
     val cacheEnvIO = for {
       counter <- Ref[IO].of(0)
+      _ <- logger.info("Set all pairs state by default")
       rates <- Ref[IO].of(CacheProgramHelper.allPairs)
       duration <- Ref[IO].of(responseAfter)
       ratesService = new OneFrameModified[IO](counter, rates, duration)
@@ -102,13 +109,16 @@ class CacheProgramStateTest extends AnyFunSuite {
       cacheEnv <- cacheEnvIO
       _ <- timer.sleep(3.seconds)
       callsBefore <- cacheEnv.counter.get
+      _ <- logger.info("Check that no any calls performed on start due to long response time from dummy")
       _ = assert(callsBefore == 0)
+      _ <- logger.info("Touch cache with two calls, that should wait for filling cache")
       rate1 <- cacheEnv.cacheService.get(List(pairUSDEUR))
       rate2 <- cacheEnv.cacheService.get(List(nonexistentPair))
+      _ <- logger.info("Check that clients receive appropriate rates")
       _ = assert(rate1.getOrElse(throw CacheProgramHelper.tryToGetLeft) == List(rateUSDEUR))
-      _ = assert(rate2.left.exists(
-        error => error == RateNotFound(errors.Messages.notFoundRateMessage(nonexistentPair))))
+      _ = assert(rate2.left.exists(error => error == RateNotFound(errors.Messages.notFoundRateMessage(nonexistentPair))))
       callsAfter <- cacheEnv.counter.get
+      _ <- logger.info("Check that frame was touched only once")
       _ = assert(callsAfter == 1)
     } yield ()
     resultsIO.unsafeRunSync()
@@ -150,7 +160,7 @@ class CacheProgramStateTest extends AnyFunSuite {
       _ <- logger.info("Sleep until expiration period finished")
       _ <- timer.sleep(2.second)
       callsInLazyAfterExpiration <- cacheEnv.counter.get
-      _ <- logger.info("Check that the same count af calls remains when expiration period finished too")
+      _ <- logger.info("Check that the same count of calls remains when expiration period finished too")
       _ = assert(callsInLazyAfterExpiration == 2)
     } yield ()
     resultsIO.unsafeRunSync()
@@ -196,7 +206,7 @@ class CacheProgramStateTest extends AnyFunSuite {
       _ = assert(rateAfterRefresh1.getOrElse(throw CacheProgramHelper.tryToGetLeft) == List(rateUSDEUR))
       _ = assert(rateAfterRefresh2.getOrElse(throw CacheProgramHelper.tryToGetLeft) == List(rateUSDAUD))
       callsInAfterLazy <- cacheEnv.counter.get
-      _ <- logger.info("Check that second call to frame ware performed")
+      _ <- logger.info("Check that only one second call to frame ware performed")
       _ = assert(callsInAfterLazy == 2)
     } yield ()
     resultsIO.unsafeRunSync()
@@ -255,7 +265,7 @@ class CacheProgramStateTest extends AnyFunSuite {
     )
     val cacheEnvIO = for {
       counter <- Ref[IO].of(0)
-      _ <- logger.info("Set empty state by default")
+      _ <- logger.info("Set all pairs state by default")
       rates <- Ref[IO].of(allPairs)
       duration <- Ref[IO].of(100.milliseconds)
       ratesService = new OneFrameModified[IO](counter, rates, duration)
@@ -265,6 +275,7 @@ class CacheProgramStateTest extends AnyFunSuite {
     val resultsIO = for {
       cacheEnv <- cacheEnvIO
       _ <- timer.sleep(3.seconds)
+      _ <- logger.info("Perform two gets")
       rateBeforeRefresh1 <- cacheEnv.cacheService.get(List(pairUSDEUR))
       rateBeforeRefresh2 <- cacheEnv.cacheService.get(List(pairUSDAUD))
       _ <- logger.info("Check that cache have rates")
@@ -274,9 +285,10 @@ class CacheProgramStateTest extends AnyFunSuite {
       _ <- cacheEnv.ratesService.modifyRates(emptyPairs)
       _ <- logger.info("Wait until refresh period finished")
       _ <- timer.sleep(configCache.refreshTimeout.plus(1.second))
-      _ <- logger.info("Check that cache have new state after refresh")
+      _ <- logger.info("Perform two gets")
       rateAfterRefresh1 <- cacheEnv.cacheService.get(List(pairUSDEUR))
       rateAfterRefresh2 <- cacheEnv.cacheService.get(List(pairUSDAUD))
+      _ <- logger.info("Check that cache have new state after refresh")
       _ = assert(rateAfterRefresh1.left.exists(error => error == RateNotFound(errors.Messages.notFoundRateMessage(pairUSDEUR))))
       _ = assert(rateAfterRefresh2.left.exists(error => error == RateNotFound(errors.Messages.notFoundRateMessage(pairUSDAUD))))
     } yield ()
